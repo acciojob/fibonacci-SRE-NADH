17246bcc6ff0d82fd43cd227ae7b998a3cec967d
SRE-NADH <95708236+SRE-NADH@users.noreply.github.com>
2023-07-14 10:01:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function fibonacci(num) {
 // your code here
 	let a=0;
      let b=1;
-	if(num==1) return a
-	if(num=2) return b
-	for(let i=0;i<num-2;i++){
+	if(num==0) return a
+	if(num==1) return b
+	for(let i=0;i<num-1;i++){
 		let ans= a+b;
 		a=b;
 		b=ans;
